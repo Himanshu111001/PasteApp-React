1cefa6a83066a0f4c339b77dfcea5037b36086f1
Himanshu111001 <shishodia1977@gmail.com>
2025-03-26 15:40:47 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./components/Home"
-import Paste from "./components/Paste"
-import ViewPaste from "./components/ViewPaste"
-import Navbar from "./components/Navbar"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
+import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
-
-const router = createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <div className="w-full h-full flex flex-col">
-        <Navbar/>
-        <Home/>
+        <Navbar />
+        <Home />
       </div>
-    },
-    {
-      path:"/pastes",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <Paste/>
-    </div>
-    },
-    {
-      path:"/pastes/:id",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <ViewPaste/>
-    </div>,
-    }
-  ]
-)
+    ),
+  },
+  {
+    path: "/pastes",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <Paste />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes/:id",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <ViewPaste />
+      </div>
+    ),
+  },
+]);
 
 function App() {
-
   return (
-    <RouterProvider router={router}/>
-  )
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
-export default App
+export default App;
